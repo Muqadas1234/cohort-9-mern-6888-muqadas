10a370e3222ee84ff83a4fb7296a683d232bcd2f
Muqadas Akram <muqadasakram.13@gmail.com>
2026-07-21 23:49:02 +0500

    Handle duplicate email race condition in signup

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,7 +1,3 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
 const registerUser = async ({ name, email, password }) => {
   const existingUser = await User.findOne({ email });
   if (existingUser) {
@@ -12,37 +8,20 @@ const registerUser = async ({ name, email, password }) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-  return user;
-};
-
-const loginUser = async ({ email, password }) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    const error = new Error('Invalid email or password');
-    error.statusCode = 401;
-    throw error;
+    return user;
+  } catch (err) {
+    if (err.code === 11000) {
+      const error = new Error('Email already in use');
+      error.statusCode = 409;
+      throw error;
+    }
+    throw err;
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    const error = new Error('Invalid email or password');
-    error.statusCode = 401;
-    throw error;
-  }
-
-  const token = jwt.sign(
-    { userId: user._id },
-    process.env.JWT_SECRET,
-    { expiresIn: '7d' }
-  );
-
-  return { user, token };
 };
-
-module.exports = { registerUser, loginUser };
